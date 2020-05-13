69e7b578723d2df3f4fcd460bb52c852db22620b
Thad House <thadhouse1@gmail.com>
2020-05-12 19:51:11 -0700

    Add azure files, and formatting

--- src/SecurityConfiguration.cs
+++ src/SecurityConfiguration.cs
@@ -39,7 +39,7 @@ namespace QuicNet
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
             // TODO: uncomment the following line if the finalizer is overridden above.
-             GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
         #endregion
     }
